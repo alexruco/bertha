08a6c7619d75ebdf8d918284224481d2fc3fe772
Alex Ruco <alex@ruco.pt>
2024-08-13 17:24:55 +0100

    success and referring being added. TODO: append referring if page exists

--- bertha/crawl_pages.py
+++ bertha/crawl_pages.py
@@ -8,7 +8,6 @@ from utils import check_http_status
 from database_setup import initialize_database
 from database_operations import insert_if_not_exists,update_crawl_info
 
-
 def crawl_pages(urls, db_name='db_websites.db', retries=5):
     """
     Crawls the provided collection of URLs, checking the status of pages and updating the database.
@@ -45,9 +44,28 @@ def crawl_pages(urls, db_name='db_websites.db', retries=5):
                         # Page is available; get internal links
                         internal_links = internal_links_on_page(url)
 
-                        # Insert each internal link if it doesn't already exist
+                        # Insert each internal link if it doesn't already exist and update referring_pages if it does
                         for link in internal_links:
-                            insert_if_not_exists(link, referring_page=url, db_name=db_name)
+                            # Check if the link already exists
+                            cursor.execute('SELECT referring_pages FROM tb_pages WHERE url = ?', (link,))
+                            row = cursor.fetchone()
+
+                            if row:
+                                existing_referring_pages = row[0]
+                                if existing_referring_pages:
+                                    new_referring_pages = f"{existing_referring_pages},{url}"
+                                else:
+                                    new_referring_pages = url
+                                
+                                cursor.execute('''
+                                    UPDATE tb_pages
+                                    SET referring_pages = ?
+                                    WHERE url = ?
+                                ''', (new_referring_pages, link))
+                                print(f"Updated 'referring_pages' for '{link}' with new referrer '{url}'.")
+                            else:
+                                # If the link doesn't exist, insert it
+                                insert_if_not_exists(link, referring_page=url, db_name=db_name)
 
                         # Update the HTTP status, dt_last_crawl, and successful_page_fetch in the database for the crawled URL
                         cursor.execute('''
